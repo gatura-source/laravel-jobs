5707e430b77020f69ce6dc0f8616a926e5c0e601
bdan007 <bedan.mwaura@aimsoft.co.ke>
2025-07-17 16:06:22 +0300

    Job application added

---
+++ app/Http/Controllers/JobApplicationsController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobApplication;
+use App\Models\Jobs;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class JobApplicationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    // public function index()
+    // {
+    //     //
+    // }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Jobs $job)
+    {
+        //
+        return view("applications.createapplication", ['job' => $job]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+     public function store(Jobs $job, Request $request)
+    {
+        $job->jobapplications()->create([
+            'user_id' => $request->user()->id,
+            ...$request->validate([
+                'expected_salary' => 'required|min:1|max:1000000'
+            ])
+        ]);
+
+        return redirect()->route('jobs.show', $job)
+            ->with('success', 'Job application submitted.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JobApplication $jobApplication)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    // public function edit(JobApplication $jobApplication)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  */
+    // public function update(Request $request, JobApplication $jobApplication)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  */
+    // public function destroy(JobApplication $jobApplication)
+    // {
+    //     //
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\JobApplicationsController;
+use Illuminate\Contracts\Queue\Job;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -22,3 +24,9 @@ Route::delete('logout', fn () => to_route('auth.destroy'))
     ->name('logout');
 Route::delete('auth', [AuthController::class, 'destroy'])
     ->name('auth.destroy');
+// Route::resource('applications', JobApplicationsController::class)
+//     ->only(['create', 'store']);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('jobs.applications', JobApplicationsController::class)
+    ->only('create','store');
+});
